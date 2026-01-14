7b5a0a939e32188539673f3b9455f84302d330e1
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-01-14 14:39:36 -0300

    config: astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marcelosarinho.github.io/portfolio/',
   vite: {
     plugins: [tailwindcss()]
   }
